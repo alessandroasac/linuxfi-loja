c60dd186fa7108c1061d84aaf58d01bbf51684df
Alessandro <alessandro.asac@gmail.com>
2010-07-15 17:52:14 -0300

    aula 4

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,7 +1,9 @@
 class Pedido < ActiveRecord::Base
   
-  has_many :itens
-  has_many :produtos, :through => :itens
+  has_many :itens, :dependent => :destroy
+  accepts_nested_attributes_for :itens
+  after_save :remover_itens_zerados
+  #  has_many :produtos, :through => :itens
   
   def adicionar_produto(produto, quantidade)
     if item = self.itens.detect { |i| i.produto == produto }
@@ -12,7 +14,24 @@ class Pedido < ActiveRecord::Base
   end
   
   def preco_total
-    self.itens.inject(0) { |acc, item| acc + item.preco_total }
+    self.itens.to_a.sum( &:preco_total )
   end
-  
+
+  def blank?
+    self.itens.blank?
+  end
+
+  private
+
+  def remover_itens_zerados
+    itens_a_remover = []
+    self.itens.each do |item|
+      puts "#{item.produto.nome}, #{item.quantidade} <<<<<<<<<<<<<<<<<<<<<<<<<<"
+      if item.quantidade.blank? || item.quantidade < 1
+        itens_a_remover << item
+      end
+    end
+    self.itens.delete( *itens_a_remover )
+  end
+
 end
