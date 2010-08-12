913b22b573357fd2260150d19828f8324af9e1ff
Alessandro <alessandro.asac@gmail.com>
2010-08-11 22:12:39 -0300

    aula 11

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+Produto.create!( [
+    { :nome => "Head First Servlets", :descricao => "Aprenda servlets", :preco => 23 },
+    { :nome => "Ruby Bible", :descricao => "A Salvação em ruby", :preco => 40 },
+    { :nome => "Scala In Action", :descricao => "Programando", :preco => 35 }
+  ] )
+
+Usuario.create!( [
+    { :nome => "José",
+      :email => "jose@email.com",
+      :senha_confirmation => "123456",
+      :senha => "123456",
+      :termos_e_condicoes => "1"
+    }
+  ])
+
+administrador = Usuario.new( :nome => "admin",
+  :email => "admin@mail.com",
+  :senha => "123456",
+  :senha_confirmation => "123456",
+  :termos_e_condicoes => "1"
+)
+
+administrador.administrador = true
+administrador.save!
